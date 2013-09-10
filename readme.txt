396ed04abbed85e90c421dc85683e432b670fa1a
ohryan <ohryan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-10 00:29:21 +0000

    First version of plugin.
    git-svn-id: https://plugins.svn.wordpress.org/retroposts/trunk@769473 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== RetroPosts ===
+Contributors: ohryan
+Tags: dashboard widget, post history
+Requires at least: 3.0
+Tested up to: 3.6
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+RetroPosts creates a dashboard widget that displays past blog posts.
+
+== Description == 
+
+It's easy to forget about the great blog posts you've written in the past. This plugin surfaces post from the current week in your blog's history. 
+
+Feature requests and ideas welcome: ryan@ohryan.ca
+
+== Installation ==
+
+1. Upload the plugin.
+2. Activate it. 
+3. Check your dashboard.
+
+== Changelog ==
+
+= 0.1 =
+* Initial version. Nothing to change. 
---
+++ retropost.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: RetroPosts
+Plugin URI: http://soon
+Description: Displays posts from the past!
+Version: 0.1
+Author: Ryan Neudorf
+Author URI: http://ohryan.ca/
+License: GPL2
+*/
+
+function retroposts_add_dashboard_widgets() {
+	wp_add_dashboard_widget(
+                 'retroposts_dashboard_widget',
+                 'RetroPosts',
+                 'retroposts_dashboard_widget_display'
+        );
+}
+
+add_action( 'wp_dashboard_setup', 'retroposts_add_dashboard_widgets' );
+
+
+function retroposts_dashboard_widget_display() {
+	$week = date('W') - 1;
+
+	add_filter( 'posts_where', 'retroposts_filter_year' );
+	add_filter( 'posts_orderby', 'retroposts_filter_order' );
+	$query = new WP_Query( "w=$week&post_status=publish&posts_per_page=-1");
+	remove_filter( 'posts_where', 'retroposts_filter_year' );
+	remove_filter( 'posts_orderby', 'retroposts_filter_order' );
+
+	$post_count = $query->post_count;
+
+	if ($query->have_posts()): ?>
+		<p>This week in the past you wrote <strong><?php echo $post_count ?></strong> posts.</p>
+		<ul>
+		<?php while ( $query->have_posts() ): $query->the_post(); ?>
+
+			<li>
+				<?php
+				if ( $loop_year != get_the_date( 'Y' ) ) {
+					$loop_year = get_the_date('Y');
+					echo "<h2>$loop_year</h2>";
+				}
+				?>
+			<h4><?php the_date('l') ?>: <a href="<?php echo the_permalink() ?>"><?php echo the_title(); ?></a></h4></li>
+
+
+		<?php endwhile; ?>
+		</ul>
+	<?php else: ?>
+		<p>You haven't written any posts during this week in the past.</p>
+	<?php endif; 
+	
+}
+
+function retroposts_filter_year( $where = '' ) {
+	$year = date('Y');
+	$where .= " AND YEAR(post_date) < $year";
+	return $where;
+}
+
+function retroposts_filter_order( $orderyby_statement ) {
+	$orderyby_statement = 'YEAR(post_date) DESC, DAYOFYEAR(post_date) ASC';
+	return $orderyby_statement;
+}
+?>
