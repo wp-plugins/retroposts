906603da01bd6630f3a46e6429a8dd1b75ea9969
ohryan <ohryan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-01 07:02:15 +0000

    Rewrote the plugin. Added a sidebar widget. Optimized WP_Query
    git-svn-id: https://plugins.svn.wordpress.org/retroposts/tags/1.0@1210787 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- retropost.php
+++ retropost.php
@@ -1,77 +1,32 @@
 <?php
 /*
-Plugin Name: RetroPosts
+Plugin Name: Historian
 Plugin URI: https://github.com/ohryan/RetroPosts
-Description: Displays posts from the past!
-Version: 0.1.1
+Description: Historian surfaces old blog posts on your dashboard and as a sidebar widget.
+Version: 1.0
 Author: Ryan Neudorf
 Author URI: http://ohryan.ca/
 License: GPLv2 or later
 */
+namespace RP;
+
+require_once 'classes/Historian.php';
+require_once 'classes/HistorianWidget.php';
 
 function retroposts_add_dashboard_widgets() {
 	wp_add_dashboard_widget(
                  'retroposts_dashboard_widget',
-                 'RetroPosts',
-                 'retroposts_dashboard_widget_display'
+                 'Historian',
+                 __NAMESPACE__.'\\retroposts_dashboard_widget_display'
         );
 }
 
-add_action( 'wp_dashboard_setup', 'retroposts_add_dashboard_widgets' );
-
+add_action( 'wp_dashboard_setup', __NAMESPACE__.'\\retroposts_add_dashboard_widgets' );
 
 function retroposts_dashboard_widget_display() {
-	$week = date('W');
-	$loop_year = null;
-
-
-
-	add_filter( 'posts_where', 'retroposts_filter_year' );
-	add_filter( 'posts_orderby', 'retroposts_filter_order' );
-	$query = new WP_Query( "w=$week&post_status=publish&posts_per_page=-1");
-	remove_filter( 'posts_where', 'retroposts_filter_year' );
-	remove_filter( 'posts_orderby', 'retroposts_filter_order' );
-
-	$post_count = $query->post_count;
-
-	if ($query->have_posts()): ?>
-		<p>This week in the past you wrote <strong><?php echo $post_count ?></strong> posts.</p>
-		<ul>
-		<?php while ( $query->have_posts() ): $query->the_post(); ?>
-
-			<li>
-				<?php
-				if ( $loop_year != get_the_date( 'Y' ) ) {
-					$loop_year = get_the_date('Y');
-					$day_of_week = '';
-					echo "<h2>$loop_year</h2>";
-				}
-
-				if ( $day_of_week != get_the_date('l') ) {
-					$day_of_week = get_the_date( 'l' );
-					echo "<h4>$day_of_week</h4>";
-				}
-				?>
-
-			<h4><a href="<?php echo the_permalink() ?>"><?php echo the_title(); ?></a></h4></li>
-
-
-		<?php endwhile; ?>
-		</ul>
-	<?php else: ?>
-		<p>You haven't written any posts during this week in the past.</p>
-	<?php endif; 
-	
+	$h = new Historian();
+	$h->displayDashboardWidget();
 }
 
-function retroposts_filter_year( $where = '' ) {
-	$year = date('Y');
-	$where .= " AND YEAR(post_date) < $year";
-	return $where;
-}
-
-function retroposts_filter_order( $orderyby_statement ) {
-	$orderyby_statement = 'YEAR(post_date) DESC, DAYOFYEAR(post_date) ASC';
-	return $orderyby_statement;
-}
+add_action('widgets_init', function(){ return register_widget(__NAMESPACE__."\\HistorianWidget"); });
 ?>
